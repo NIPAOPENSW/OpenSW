856faa65224428ecde196b506a6e35a5535e9be1
pyeong <gipyong7@gmail.com>
2023-10-19 11:30:00 +0900

    Replace spaces with enter

--- main_final_ui.py
+++ main_final_ui.py
@@ -308,7 +308,7 @@ class Ui_MainWindow(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Gulim\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
-        self.header_text.setPlaceholderText(_translate("MainWindow", "header1 = value1                                                                                                                                                                                                                                                                                    header2 = value2                                                                                                                                                                                                                                                                                    header3 = value3"))
+        self.header_text.setPlaceholderText(_translate("MainWindow", "header1 = value1\nheader2 = value2\nheader3 = value3"))
         self.parameter_menu.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
